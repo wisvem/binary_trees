10bdf6aa230c7c1e238b5804953de65a204a03fd
Wiston Venera Macías <wisvem@hotmail.com>
2021-03-08 22:47:50 +0000

    Task 2 - Function to add node at right

---
+++ 2-binary_tree_insert_right.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_insert_right -  inserts a node as the right-child of another node
+* @parent: pointer to the node to insert the right-child in
+* @value: value to put in the new node
+* Return: a pointer to the created node
+* or NULL on failure or if parent is NULL
+*/
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode = NULL;
+
+	newnode = malloc(sizeof(binary_tree_t));
+	if (newnode == NULL || parent == NULL)
+	{
+		return (NULL);
+	}
+	(*newnode).n = value;
+	(*newnode).left = NULL;
+	(*newnode).right = NULL;
+
+	if ((*parent).right != NULL)
+	{
+		(*newnode).parent = parent;
+		(*newnode).right = (*parent).right;
+		(*parent).right = newnode;
+		(*(*newnode).right).parent = newnode;
+	}
+	else
+	{
+		(*parent).right = newnode;
+		(*newnode).parent = parent;
+	}
+	return (newnode);
+}
