602e8f93263786393e413af7b91012bf19cb9b89
Wiston Venera Macías <wisvem@hotmail.com>
2021-03-09 20:34:40 +0000

    Task 9 - Fixed case when root is null

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -1,27 +1,38 @@
 #include "binary_trees.h"
 
 /**
+* scale - aux function that measures the height of a binary tree
+* @tree: pointer to the root node of the tree to measure the height
+* Return: height of a binary tree, 0 is tree is NULL
+**/
+int scale(const binary_tree_t *tree)
+{
+	int ll = 1;
+	int lr = 1;
+
+	if (tree == NULL)
+	{
+		return (-1);
+	}
+	ll += scale((*tree).left);
+	lr += scale((*tree).right);
+	if (lr > ll)
+	{
+		return (lr);
+	}
+	return (ll);
+}
+
+/**
 * binary_tree_height - function that measures the height of a binary tree
 * @tree: pointer to the root node of the tree to measure the height
 * Return: height of a binary tree, 0 is tree is NULL
 **/
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	int ll;
-	int lr;
-
 	if (tree == NULL)
 	{
-		return (-1);
+		return (0);
 	}
-	ll = binary_tree_height((*tree).left);
-	lr = binary_tree_height((*tree).right);
-	if (lr > ll)
-	{
-		return (lr + 1);
-	}
-	else
-	{
-		return (ll + 1);
-	}
+	return (scale(tree));
 }
